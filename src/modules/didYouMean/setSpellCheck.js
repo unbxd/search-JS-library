7e521b8da3f9f809f56ab553767b951a283c8d54
Manu Sunny <manu.sunny@unbxd.com>
2020-07-06 19:03:54 +0530

    small bug fix and updated with latest code

--- src/modules/didYouMean/setSpellCheck.js
+++ src/modules/didYouMean/setSpellCheck.js
@@ -1,12 +1,23 @@
 import renderDidYouMean from './renderSpellCheck';
 
+//will be removed once it is fixed in js core
+const getDidYouMeanFromResponse = function() {
+    const responseObj = this.getResponseObj()||{};
+    if(responseObj && responseObj.didYouMean) {
+        return responseObj.didYouMean
+    }
+    return null;
+
+}
 const setSpellCheck = (prototype) => {
     prototype = Object.assign(prototype,{
-        renderDidYouMean
+        renderDidYouMean,
+        getDidYouMeanFromResponse
     })
 }
 
 export {
     setSpellCheck as default,
-    renderDidYouMean
+    renderDidYouMean,
+    getDidYouMeanFromResponse
 };
