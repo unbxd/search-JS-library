0fd92b11eae37e300044408bb48a5fd28bfcf9f1
Manu Sunny <manu.sunny@unbxd.com>
2020-08-27 18:29:32 +0530

    magentofixes for the error

--- src/core/initialize.js
+++ src/core/initialize.js
@@ -1,14 +1,11 @@
 
-import events from '../common/constants/eventsLib';
-import actions from '../common/constants/actions';
+
 import unxSelectors from '../common/constants/selectors';
 import reRender from './reRender';
 import bindEvents from './bindEvents';
 import createLayout from '../core/createLayout';
 
 const initialize = function() {
-    this.events = events;
-    this.actions = actions;
     this.createLayout = createLayout.bind(this);
     this.createLayout();
     this.unxSelectors = unxSelectors;
@@ -18,7 +15,6 @@ const initialize = function() {
     this.reRender = reRender.bind(this);
     this.bindEvents = bindEvents.bind(this);
     this.bindEvents();
-    this.callBack = this.callBack.bind(this);
     const urlParams = this.getQueryParams();
     const ln = Object.keys(urlParams).length;
     if(ln > 0){
