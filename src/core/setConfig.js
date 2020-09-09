226eaefbe89ddc62e7da9eeab3329a0d7134dfb4
Manu Sunny <manu.sunny@unbxd.com>
2020-09-09 12:29:22 +0530

    set config issue

--- src/core/setConfig.js
+++ src/core/setConfig.js
@@ -22,6 +22,9 @@ const setConfig = function(options = {}, props = {}) {
     if(products) {
         this.options.productAttributes = (products.productAttributes) ? products.productAttributes : options.products.productAttributes;
         this.options.productType = (products.productType) ? products.productType : options.products.productType;
+    } else {
+        this.options.productType = options.products.productType;
+        this.options.productAttributes = options.products.productAttributes;
     }
     if(swatches) {
         this.options.showSwatches = (swatches && swatches.enabled) ? swatches.enabled :options.swatches.enabled;
--- src/modules/didYouMean/renderSpellCheck.js
+++ src/modules/didYouMean/renderSpellCheck.js
@@ -8,6 +8,10 @@ const renderDidYouMean = function (suggestion) {
     const {
         type
     } = this.options.pagination;
+    const {
+        spellCheck,
+        enabled
+    } = this.options.spellCheck;
     if(type !== "FIXED_PAGINATION" && pages) {
         const {
             start,
@@ -17,6 +21,26 @@ const renderDidYouMean = function (suggestion) {
         pages.start = 0;
     }
     let sugString = didYouMean.length >0 ? didYouMean[0].suggestion :"";
-    return this.options.spellCheck.template.bind(this)(userInput,sugString,pages);
+    if(enabled) {
+        if(sugString ) {
+            if(this.viewState.lastDidYouMean !== userInput) {
+                this.viewState.lastDidYouMean = userInput;
+                this.getResults(sugString);
+            }
+        }
+        if(this.viewState.lastDidYouMean && sugString ==="" && window.unbxdSearch.viewState.loadedFromSuggestion){
+            this.viewState.lastDidYouMean = null;
+            window.unbxdSearch.viewState.loadedFromSuggestion = false;
+            return this.options.spellCheck.template.bind(this)(userInput,sugString,pages)
+        }
+        if(this.viewState.lastDidYouMean && sugString ==="") {
+            return this.options.spellCheck.template.bind(this)(userInput,this.viewState.lastDidYouMean,pages);
+        }else {
+            return this.options.spellCheck.template.bind(this)(userInput,sugString,pages)
+        }
+        
+    }else {
+        return this.options.spellCheck.template.bind(this)(userInput,sugString,pages);
+    }
 }
 export default renderDidYouMean;
