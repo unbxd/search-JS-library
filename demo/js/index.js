28da91fab73c6f15ef0e2a0473a4e3c0d2e90e0b
Manu Sunny <manu.sunny@unbxd.com>
2020-09-25 13:42:26 +0530

    updated read me

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,5 +1,67 @@
 import UnbxdSearch from  "../../src/index";
+import 'nouislider/distribute/nouislider.css';
+import noUiSlider from 'nouislider';
 
+
+const setCategory = function (e) {
+  const el = e.target;
+  const { dataset } = el;
+  console.log(dataset, "dataset");
+  if (dataset && dataset.id) {
+    window.UnbxdAnalyticsConf = {
+      page: dataset.id
+    };
+    window.unbxdSearch.getCategoryPage();
+  }
+};
+const navElem = document.getElementById("categoryLinks");
+navElem.addEventListener("click", setCategory);
+
+
+const rangeTemplate = function(range, selectedRange, facet) {
+
+  const {
+      displayName,
+      facetName,
+      values,
+      gap
+    } = range;
+    let valueUI = ``;
+    const {
+      facetClass,
+      selectedFacetClass,
+      applyMultipleFilters,
+      applyButtonText,
+          clearButtonText,
+    } = facet;
+    const selected = selectedRange.length > 0 ? true :false;
+    values.forEach(item =>{
+        const {
+          from,
+          end
+        } = item;
+      const isSelected = this.isSelectedRange(facetName,item);
+      const btnCss = (isSelected) ? `UNX-selected-facet-btn ${facetClass} ${selectedFacetClass}`:`${facetClass}`;
+      valueUI +=[`<button class="${btnCss} UNX-range-facet UNX-change-facet" data-action="setRange" data-facet-name="${facetName}" data-start="${from.dataId}" data-end="${end.dataId}" >`,
+          `<span class="UNX-facet-text">${from.name}  -  ${end.name}</span>`,
+          `<span class="UNX-facet-count">(${from.count})</span>`,
+    `</button>`].join('');
+    });
+    let clearBtn = ``;
+      let applyBtn = ``;
+      if(selected) {
+          if(applyMultipleFilters) {
+              applyBtn = `<button class="UNX-default-btn ${facetClass} UNX-facet-primary" data-action="applyRange"> ${applyButtonText}</button>`;
+          }
+          clearBtn = `<button class="UNX-default-btn UNX-facet-clear  ${facetClass}" data-action="clearRangeFacets">${clearButtonText}</button>`;
+      }
+  return [`<div class="UNX-range-wrapper">`,
+      valueUI,
+      `<div class="UNX-price-action-row">`,
+          applyBtn,clearBtn,
+      `<div>`,
+    `</div>`].join('')
+}
 
 
 const unbxdCallbackEcma = function (instance, type,data){
@@ -43,13 +105,16 @@ btnEls.forEach(item=> {
     item.addEventListener("click", toggleMobileFacets)
 })
 window.unbxdSearch = new UnbxdSearch({
-  siteKey: "demo-german-unbxd809051586180937",
-  apiKey: "16e3cf8d510c50106d64f1ebb919b34e",
+  siteKey: "demo-unbxd700181503576558",
+  apiKey: "fb853e3332f2645fac9d71dc63e09ec1",
   hashMode:true,
   updateUrls:true,
   searchBoxEl: document.getElementById("unbxdInput"),
   searchTrigger: "click",
-  searchButtonEl: document.getElementById("searchBtn")
+  searchButtonEl: document.getElementById("searchBtn"),
+  products:{
+    productType:"SEARCH",
+  }
 });
 window.unbxdSearch.updateConfig({
   products:{
@@ -57,27 +122,7 @@ window.unbxdSearch.updateConfig({
     productType:"SEARCH",
     productClick: function(product,e) {
       console.log(product,"product,index",e);
-    },
-    attributesMap:{
-      "unxPrice":"displaySalePrice"
-    },
-    productAttributes: [
-      "title",
-      "uniqueId",
-      "price",
-      "sku",
-      "imageUrl",
-      "displaySalePrice",
-      "salePrice",
-      "sortPrice",
-      "productDescription",
-      "unbxd_color_mapping",
-      "colorName",
-      "color",
-      "size",
-      "listPrice",
-      "autosuggest"
-    ]
+    }
   },
   spellCheck: {
     enabled: true,
@@ -92,64 +137,108 @@ window.unbxdSearch.updateConfig({
     applyMultipleFilters:false,
     defaultOpen:"FIRST",
     onFacetLoad:function(facets){
+      const self = this;
+      const {
+        facet
+      } = this.options;
+      const {
+        rangeWidgetConfig
+      } = facet;
+      facets.forEach(facetItem => {
+        const {
+          facetType,
+          facetName,
+          gap
+        } = facetItem;
+        const {
+          prefix
+        } = rangeWidgetConfig;
+
+        if(facetType === "range") {
+          const rangeId = `${facetName}_slider`;
+          const sliderElem = document.getElementById(rangeId);
+          let {
+            end,
+            gap,
+            max,
+            min,
+            start
+          } = facetItem;
+          const selectedValues = sliderElem.dataset;
+          if(selectedValues) {
+            start = Number(selectedValues.x),
+            end = Number(selectedValues.y)
+          }
+          this[rangeId] = noUiSlider.create(sliderElem, {
+            start: [start, end],
+            tooltips: [{
+              to: function (value) {
+                  return `${prefix} ${Math.round(value)}`;
+              }},
+              {
+                to: function (value) {
+                    return `${prefix} ${Math.round(value)}`;
+                }
+              }
+            ],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': max
+            },
+            format: {
+                to: function (value) {
+                    return Math.round(value);
+                },
+                from: function (value) {
+                    return Math.round(value);
+                }
+            },
+            padding: 0,
+            margin: 0,
+        });
+        this[rangeId].on("set", function(data){
+          const newData = {
+              start: data[0],
+              end: data[1],
+              facetName,
+              gap
+          };
+          self.setRangeSlider(newData);
+      });
+
+      }
+
+      });
     },
     isCollapsible:true,
     isSearchable:true,
     enableViewMore:false,
-    rangeTemplate:function(ranges,selectedRanges) {
-      let ui  = ``;
+    rangeTemplate: function(range, selectedRange, facet){
       const {
-        selectedFacetClass,
-        facetClass,
-        applyMultipleFilters
-      } = this.options.facet;
-      let selected = false;
-      ranges.forEach(range => {
-          const {
-            displayName,
-            facetName,
-            values
-          } = range;
-          let valueUI = ``;
-          selected = (selectedRanges[facetName]) ? true :false;
-          values.forEach(item =>{
-              const {
-                from,
-                to,
-                end
-              } = item;
-            const isSelected = this.isSelectedRange(facetName,item);
-            const last = end || to;
-
-            const btnCss = (isSelected) ? `UNX-selected-facet-btn ${facetClass} ${selectedFacetClass}`:`${facetClass}`;
-            valueUI +=[`<button class="${btnCss} UNX-range-facet UNX-change-facet" data-action="setRange" data-facet-name="${facetName}" data-start="${from.dataId}" data-end="${last.dataId}" >`,
-                `<span class="UNX-facet-text">${from.name}  -  ${last.name}</span>`,
-                `<span class="UNX-facet-count">(${from.count})</span>`,
-          `</button>`].join('');
-          });
-          ui += [`<div class="UNX-facets-inner-wrapper">`,
-            `<h3 class="UNX-facet-header">${displayName}</h3>`,
-                `<div class="UNX-facets">${valueUI}</div>`,
-          `</div>`].join('');
-      });
-      let clearBtn = ``;
-      let applyBtn = ``;
-      if(selected) {
-          if(applyMultipleFilters) {
-              applyBtn = `<button class="UNX-default-btn ${facetClass} UNX-facet-primary" data-action="applyRange"> Apply</button>`;
-          }
-          clearBtn = `<button class="UNX-default-btn UNX-facet-clear  ${facetClass}" data-action="clearRangeFacets"> clear</button>`;
+        facetName,
+        start,
+        end
+      } = range;
+      let min = start;
+      let max = end;
+      if(selectedRange.length > 0) {
+        const sel  = selectedRange[0].replace(/[^\w\s]/gi, '').split(" TO ");
+        min = sel[0];
+        max = sel[1];
       }
-      return [`<div class="UNX-range-wrapper">`,
-        ui,
-        `<div class="UNX-price-action-row">`,
-            applyBtn,clearBtn,
-        `<div>`,
-      `</div>`].join('')
+      console.log(max,"max")
+      const rangId = `${facetName}_slider`;
+      return [`<div id="${facetName}"  data-id="${facetName}" class=" UNX-range-slider-wrap">`,
+                      `<div class="UNX-value-container UNX-range-value-block" ></div>`,
+                      `<div id="${rangId}" data-x="${min}" data-y="${max}" class="UNX-range-slider-wrapper"></div>`,
+                  `</div>`,
+              `<div>`,
+          `</div>`].join('')
     }
   },
   pagination: {
-    type:'FIXED_PAGINATION',
+    type:'INFINITE_SCROLL',
     el:document.querySelector("#clickScrollContainer"),
     onPaginate:function(data) {
     }
@@ -165,11 +254,11 @@ window.unbxdSearch.updateConfig({
     el: document.getElementById("sortWrapper"),
     options: [
       {
-        value: "sortPrice desc",
+        value: "min_price desc",
         text: "Price High to Low"
       },
       {
-        value: "sortPrice asc",
+        value: "min_price asc",
         text: " Price Low to High"
       }
     ]
