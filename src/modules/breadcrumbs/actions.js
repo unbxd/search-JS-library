f309b1ce6e4e7696cb2837af681ab92bca504e4d
Priyal Jain <priyal.jain@unbxd.com>
2023-04-10 12:41:25 +0530

    Breadcrumbs initialization changes

--- src/modules/breadcrumbs/actions.js
+++ src/modules/breadcrumbs/actions.js
@@ -1,15 +1,16 @@
 function onBreadCrumbClick(e){
 };
 function renderBreadCrumbs() {
-    let selectedCategories = this.getSelectedMultilevelFacet();
+    let selectedCategories = this.getSelectedMultilevelFacet() || [];
+
     if(this.options.products.productType === 'CATEGORY' && window.unbxdAnalyticsConf?.page?.indexOf("categoryPath") !== -1){
         selectedCategories = ['categoryPath']
     }
     let ui = ``;
     selectedCategories.forEach((item) => {
-        const data = this.getBreadCrumbsList(item);
+        const data = this.getBreadCrumbsList(item) || [];
         const {
-            breadcrumb
+            breadcrumb = {}
         } = this.options;
         ui += this.options.breadcrumb.template(data, breadcrumb);
     });
