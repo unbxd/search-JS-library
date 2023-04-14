0808b21f721854e6fa5a7096a36a6f89e3eaf919
Priyal Jain <priyal.jain@unbxd.com>
2023-04-14 10:19:35 +0530

    Pagination changes

--- src/modules/pagination/actions.js
+++ src/modules/pagination/actions.js
@@ -9,58 +9,64 @@ const triggerNextPage = (context,next, action) =>{
     });
 };
 function renderNewResults(action) {
-    const pageInfo = this.getPaginationInfo();
-    const {
-        pagination,
-        onEvent
-    } = this.options;
-    const {
-        start,
-        productsLn,
-        numberOfProducts,
-        rows,
-        isNext,
-        isPrev
-    } = pageInfo;
-    if(pagination.type === "CLICK_N_SCROLL" ) {
-        const next = start+rows;
-        if(isNext){
-            this.viewState.isInfiniteStarted = true;
-            triggerNextPage(this,next);
-        }
-    } else if(pagination.type === "INFINITE_SCROLL") {
-        if(action === this.actions.next){
-            const next = start+rows;
-            if(isNext){
+    try{
+        const pageInfo = this.getPaginationInfo() || {};
+        const {
+            pagination = {},
+            onEvent
+        } = this.options;
+        const {
+            start,
+            productsLn,
+            numberOfProducts,
+            rows,
+            isNext,
+            isPrev
+        } = pageInfo;
+        if (pagination.type === "CLICK_N_SCROLL") {
+            const next = start + rows;
+            if (isNext) {
                 this.viewState.isInfiniteStarted = true;
-                triggerNextPage(this,next, action);
+                triggerNextPage(this, next);
             }
-        }
-        if(action === this.actions.prev){
-            const prev = start-rows;
-            if(isPrev){
-                this.viewState.isInfiniteStarted = true;
-                triggerNextPage(this,prev, action);
+        } else if (pagination.type === "INFINITE_SCROLL") {
+            if (action === this.actions.next) {
+                const next = start + rows;
+                if (isNext) {
+                    this.viewState.isInfiniteStarted = true;
+                    triggerNextPage(this, next, action);
+                }
             }
-        }
-    } else {
-        if(action === this.actions.next){
-            const next = start+rows;
-            if(isNext){
-                triggerNextPage(this,next);
+            if (action === this.actions.prev) {
+                const prev = start - rows;
+                if (isPrev) {
+                    this.viewState.isInfiniteStarted = true;
+                    triggerNextPage(this, prev, action);
+                }
             }
-        }
-        if(action === this.actions.prev){
-            const prev = start-rows;
-            if(isPrev){
-                this.viewState.lastAction = "pagination";
-                triggerNextPage(this,prev);
-                onEvent(this,this.events.pagePrev, {
-                    value:prev
-                });
+        } else {
+            if (action === this.actions.next) {
+                const next = start + rows;
+                if (isNext) {
+                    triggerNextPage(this, next);
+                }
             }
-        }
+            if (action === this.actions.prev) {
+                const prev = start - rows;
+                if (isPrev) {
+                    this.viewState.lastAction = "pagination";
+                    triggerNextPage(this, prev);
+                    onEvent(this, this.events.pagePrev, {
+                        value: prev
+                    });
+                }
+            }
 
+        }
+    }
+    catch(err){
+        this.options.onError("Pagination",err);
+        throw err;
     }
 };
 function paginationAction(e){
