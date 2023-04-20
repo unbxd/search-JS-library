3ef007e858ade98b98e717fa95ed13c8a125f3db
Priyal Jain <priyal.jain@unbxd.com>
2023-04-21 00:57:39 +0530

    Added try/catch in setInputValue

--- src/modules/input/setInputValue.js
+++ src/modules/input/setInputValue.js
@@ -1,23 +1,28 @@
 const setInputValue = function(e) {
-    let val = this.options.searchBoxEl.value;
-    if (!val.replace(/\s/g, '').length) {
-        return false;
-    }
-    this.options.productType = "SEARCH";
-    val = (val.indexOf("#") === 0) ? val.replace("#",""):val;
-    if(val) {
-        this.resetAll();
-        this.changeInput(val, this.events.changeInput);
-        this.options.products.productType = "SEARCH";
-        this.viewState.loadedFromSuggestion = false;
-        this.state.selectedSort = "";
-        this.setPageStart(0);
-        this.resetViewState();
-        this.getResults();
+    try{
+        let val = this.options.searchBoxEl.value ;
+        if (!val.replace(/\s/g, '').length) {
+            return false;
+        }
+        this.options.productType = "SEARCH";
+        val = (val.indexOf("#") === 0) ? val.replace("#", "") : val;
+        if (val) {
+            this.resetAll();
+            this.changeInput(val, this.events.changeInput);
+            this.options.products.productType = "SEARCH";
+            this.viewState.loadedFromSuggestion = false;
+            this.state.selectedSort = "";
+            this.setPageStart(0);
+            this.resetViewState();
+            this.getResults();
 
-    } else{
-        this.searchResultsWrapper.innerHTML = null;
+        } else {
+            this.searchResultsWrapper.innerHTML = null;
+        }
+    }catch(err){
+        this.onError("input > setInputValue.js",err)
     }
+    
 }
 export {
     setInputValue as default
