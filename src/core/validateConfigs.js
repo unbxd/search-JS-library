fc4559371119b0913e37dfdd760e20c0c3f88fce
Priyal Jain <priyal.jain@unbxd.com>
2023-05-08 17:16:08 +0530

    Changes for schema

--- src/core/validateConfigs.js
+++ src/core/validateConfigs.js
@@ -1,17 +1,18 @@
-import { isNode, isElement, isBoolean, isStr, isObj, isFunc, isArrObj } from "../common/utils";
-import { paginationSchema,
-        bannerSchema, 
-        loaderSchema, 
-        sortingSchema, 
-        noResultsSchema,
-        swatchesSchema,
-        facetsSchema, 
-        spellCheckSchema,
-        pageSizeSchema,
-        productsSchema,
-        productViewSchema,
-        breadcrumbSchema,
-        others
+import { isNode, isElement } from "../common/utils";
+import {
+    paginationSchema,
+    bannerSchema,
+    loaderSchema,
+    sortingSchema,
+    noResultsSchema,
+    swatchesSchema,
+    facetsSchema,
+    spellCheckSchema,
+    pageSizeSchema,
+    productsSchema,
+    productViewSchema,
+    breadcrumbSchema,
+    others
 } from "./configSchema";
 
 function validateConfigs() {
@@ -28,71 +29,60 @@ function validateConfigs() {
         pagesize,
         products,
         productView,
-        
+
         unbxdAnalytics
     } = this.options
 
     const validate = (userConfig = {}, schema = {}) => {
         const { moduleName = "", config = {} } = schema;
-        
+
         Object.keys(config).map(key => {
             let { datatype, required = false, allowedOptions = [], customValidations = null } = config[key];
 
             if (typeof required === "function") {
                 required = required(userConfig)
             }
-            
+
             //Checking if the datatype in user input config is not equal to that in metadata.
             if (typeof userConfig[key] !== datatype || (datatype === "object" && userConfig[key] === null)) {
-                if(datatype === "element" || datatype === "array"){
-                    if(datatype === "element"){
+                if (datatype === "element" || datatype === "array") {
+                    if (datatype === "element") {
                         //The config is required , but the user passed either null/undefined , or the element is not present on the DOM.
-                        if (required && ((!userConfig[key] || !isNode(userConfig[key]) || !isElement(userConfig[key])))) {
-                            this.onError(moduleName, `'${key}' is not a valid DOM selector`)
+                        if (required && (!userConfig[key] || !isNode(userConfig[key]) || !isElement(userConfig[key]))) {
+                            if (userConfig[key] === window) {
+                                return null
+                            }
+                            this.onError(moduleName, `'${key}' sssis not a valid DOM selector`,)
                         } else if (!required && userConfig[key] && (!isNode(userConfig[key]) || !isElement(userConfig[key]))) {
                             //The config is not required , so in case user passed null/undefined it should not throw error.
                             //Config is not required , throws error if still passed and not a valid selector
-                            this.onError(moduleName, `'${key}' is not a valid DOM selector`)
+                            this.onError(moduleName, `'${key}' is not a valid DOM selector`,)
                         }
-                    } else if (datatype === "array" && !Array.isArray(userConfig[key])){
-                        this.onError(moduleName, `'${key}' datatype should be a ${datatype}`)
-                    } 
-                }else{
-                    this.onError(moduleName, `'${key}' datatype should be a ${datatype}`)
-                }   
-                // if ((datatype === "element") || datatype === "array") {
-                //     if (datatype === "element" && !required && userConfig[key] === null){
-                        
-                //     }else if (datatype === "element" && (userConfig[key] !== window && (!isNode(userConfig[key]) || !isElement(userConfig[key])))) {
-                //         this.onError(moduleName, `'${key}' is not a valid DOM selector`)
-                //     } else if (datatype === "array" ) {
-                        
-                //     }
-                // }
-                // else {
-                //     this.onError(moduleName, `'${key}' datatype should be a ${datatype}`)
-                // }
+                    } else if (datatype === "array" && !Array.isArray(userConfig[key])) {
+                        this.onError(moduleName, `'${key}' should be of ${datatype} datatype`,)
+                    }
+                } else {
+                    this.onError(moduleName, `'${key}' should be of ${datatype} datatype`,)
+                }
             }
-            
+
             if (required && !userConfig[key] && datatype !== "element") {
-                this.onError(moduleName, `'${key}' is required`)
+                this.onError(moduleName, `'${key}' is required`,)
             }
-            
+
             if (allowedOptions.length && !allowedOptions.includes(userConfig[key])) {
-                this.onError(moduleName, `Only ${allowedOptions.join(', ')} are allowed for '${key}'`)
+                this.onError(moduleName, `Only ${allowedOptions.join(', ')} are allowed for '${key}'`,)
             }
-            
             //Checking for custom validations if any .
-            if(customValidations){
-                
-            } 
+            if (customValidations) {
+                customValidations(userConfig)
+            }
 
         })
 
     }
-
-    validate(banner, bannerSchema)
     validate(pagination, paginationSchema)
+    validate(banner, bannerSchema)
     validate(sort, sortingSchema)
     validate(noResults, noResultsSchema)
     validate(swatches, swatchesSchema)
@@ -101,10 +91,10 @@ function validateConfigs() {
     validate(facet, facetsSchema)
     validate(this.options, others)
     validate(loader, loaderSchema)
-    validate(noResults,noResultsSchema)
-    validate(products,productsSchema)
-    validate(productView,productViewSchema)
-    validate(breadcrumb,breadcrumbSchema)
+    validate(noResults, noResultsSchema)
+    validate(products, productsSchema)
+    validate(productView, productViewSchema)
+    validate(breadcrumb, breadcrumbSchema)
 
 }
 
